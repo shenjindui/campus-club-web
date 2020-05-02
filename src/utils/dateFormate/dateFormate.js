5ef4cc701942a5d252d95dfbfd6b1ef320a5d640
shenjindui <2802630961@qq.com>
2020-05-02 23:01:09 +0800

    #对事件js的修改

--- src/utils/dateFormate/dateFormate.js
+++ src/utils/dateFormate/dateFormate.js
@@ -1,14 +1,31 @@
 export default {
+    /**
+     * 对更新时间的翻译
+     * @param row
+     * @param column
+     * @returns {string}
+     */
     dateformatUpdateTime: function (row, column) {
         let date = new Date(row.updateTime).toJSON();
         return new Date(+new Date(date) + 8 * 3600 * 1000).toISOString().
         replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
     },
+    /**
+     * 对创建时间的翻译
+     * @param row
+     * @param column
+     * @returns {string}
+     */
     dateformatCreateTime: function (row, column) {
         let date = new Date(row.createTime).toJSON();
         return new Date(+new Date(date) + 8 * 3600 * 1000).toISOString().
         replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
     },
+    /**
+     * 对普通时间的翻译
+     * @param dateString
+     * @returns {string}
+     */
     dateformat: function (dateString) {
         let date = new Date(dateString).toJSON();
         return new Date(+new Date(date) + 8 * 3600 * 1000).toISOString().
